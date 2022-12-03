50f35d065aabf7cae6346690c076ae3ee5740b64
2mal3 <56305732+2mal3@users.noreply.github.com>
2022-12-03 22:00:23 +0100

    Fourier-Transformation ENDLICH

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
+import 'dart:typed_data';
 import 'package:flutter/material.dart';
-import 'package:record/record.dart';
+import 'package:fftea/fftea.dart';
+import 'package:android_path_provider/android_path_provider.dart';
+import 'package:wav/wav.dart';
+import 'dart:math';
+import 'dart:core';
 
 class ListItem extends StatelessWidget {
   const ListItem({super.key});
@@ -48,6 +53,20 @@ class MyApp extends StatelessWidget {
       ),
     );
   }
+}
+
+void read_file() async {
+  var documentsPath = await AndroidPathProvider.documentsPath;
+   final wav = await Wav.readFile("$documentsPath/sound4.wav");
+   final sound2 = wav.channels.first;
+
+  List<double> audio = sound2;
+
+  final fft = FFT(audio.length);
+  final spectrum = fft.realFft(audio).toRealArray();
+
+  final maxFreq = spectrum.indexOf(spectrum.sublist(0, 20000).reduce(max));
+  print("frequency $maxFreq ... ${fft.frequency(maxFreq, 48000)}");
 }
 
 void main() {
